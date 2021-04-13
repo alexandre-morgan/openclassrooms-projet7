edab043701ac667732a7a96d0e9596b5567db1a2
Alexandre MORGAN <alexandre.morgan.pro@gmail.com>
2021-04-13 11:50:14 -0400

    F-new component/B-new server/connection ok

--- frontend/cli-version/src/main.js
+++ frontend/cli-version/src/main.js
@@ -1,6 +1,9 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VueResource from 'vue-resource'
 import router from './router'
+
+Vue.use(VueResource)
 
 Vue.config.productionTip = false
 
---
+++ frontend/cli-version/src/views/Server.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="server" id="server-page">
+    <h1>This is the server page</h1>
+    <p> {{ users }} </p>
+  </div>
+</template>
+
+<script>
+export default {
+    data ()  {
+        return {
+            users:[]
+        }
+    },
+    mounted () {
+        this.$http.get('http://localhost:3000/api/stuff').then((response) => {
+            console.log('success', response)
+        }), (response) => {
+            console.log('erreur', response)
+        }
+    }
+}
+</script>
